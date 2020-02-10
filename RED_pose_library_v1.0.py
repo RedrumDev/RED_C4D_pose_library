46fde63d02640802466ad07bab0209b8b321c3c6
DaCosta <RedrumDev@gmail.com>
2020-02-10 17:01:03 +0100

    RED_pose_tool pseudo code

---
+++ RED_pose_library_v1.0.py
@@ -0,0 +1,29 @@
+
+import c4d
+
+
+print ("test2")
+
+"""
+Stap 1 
+After the user clicks on the button "Iniate character T pose" 
+The tool 
+A) Grabs the selected root_object of your character (it will include geoetry and controlelrs)
+B) The tool will first ask "what is your current controllers prefix?".... for example "CTRL" 
+C) The tool will ask "give your character a name"
+D) The tool will ask "In what folder do you wish to store your "character name naam" library? 
+
+
+Functions: 
+After initializing the character
+F1) Generate the folder + export an 1 frame FBX inclusing teh keyframes that are set on the current frame
+F2) It will store a thumbnail (T-pose) of your character
+F3) 
+"""
+
+
+
+"""
+Stap 2 
+Grab the selected root_object of your character (it will include geoetry and controlelrs)
+"""
